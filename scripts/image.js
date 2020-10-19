9ece3e165be642d8cac74fdfb5521552c714d4e3
Akshay Patange <akki.patange29@gmail.com>
2020-10-19 08:03:56 +0530

    img

--- scripts/image.js
+++ scripts/image.js
@@ -1,4 +1,4 @@
 let img = document.createElement('img');
-img.src = 'https://akshaypatange.github.io/images/apple.jpg'
+img.src = './images/apple.jpg'
 img.alt = 'No image'
 document.body.appendChild(img)
